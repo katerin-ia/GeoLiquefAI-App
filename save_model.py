11d9eadf6ec1d6c245b4d7c63c7648903be88cd3
Katerin De la Cruz <katerin17delacruz@gmail.com>
2025-11-19 10:56:51 -0500

    Update save_model.py

--- save_model.py
+++ save_model.py
@@ -94,7 +94,7 @@ def main():
 
     model = RandomForestClassifier(
         n_estimators=400,
-        max_depth=None,
+        max_depth=10,
         min_samples_split=2,
         min_samples_leaf=1,
         random_state=42,
